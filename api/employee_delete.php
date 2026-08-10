cd16fa51562b5d29d8ce087221c297a558ee67e8
Nanthajid Sawasri <nanthajid@gmail.com>
2026-08-10 15:30:32 +0700

    fix: ย้าย header ก่อน requireLoginApi() เพื่อแก้ session issue

--- api/employee_delete.php
+++ api/employee_delete.php
@@ -1,8 +1,8 @@
 <?php
-require_once __DIR__ . '/../includes/auth.php';
-requireLoginApi();
 header('Content-Type: application/json; charset=utf-8');
 require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../includes/auth.php';
+requireLoginApi();
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405);
